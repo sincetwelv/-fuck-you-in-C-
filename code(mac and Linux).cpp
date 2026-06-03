c42a724290248a143e685ce4df3214dd319601b1
sincetwelv <waquarjawed14@gmail.com>
2026-06-03 19:25:55 +0530

    Le Code (Mac and Linux accent)

---
+++ code(mac and Linux).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Please enter name ";
+    std::cin >> name;
+
+    std::cout << "fuck you " << name << std::endl;
+    
+    system ("read");
+return 0;
+}
