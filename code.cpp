83b76cea792951c2bc46a50a6deb639f5ce20ef2
sincetwelv <waquarjawed14@gmail.com>
2026-06-03 17:55:57 +0530

    Le Code

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int main() {
+    std::string name;
+    std::cout << "Please enter name ";
+    std::cin >> name;
+
+    std::cout << "fuck you " << name << std::endl;
+    
+    system ("pause");
+return 0;
+}
